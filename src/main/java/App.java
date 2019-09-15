c71cc075622f7b1c16bb6ae46518625c3e2348de
Omoyi <urakhel7@gmail.com>
2019-09-15 15:21:43 +0200

    add basic html that will be loaded on our page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
+    public static void main(String[] args) {
         staticFileLocation("/public");
 
     }
