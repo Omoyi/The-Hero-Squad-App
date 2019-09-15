0fb4c8684ca5cc968661341a51510d11a77d823c
Omoyi <urakhel7@gmail.com>
2019-09-15 21:01:26 +0200

    link a file into a button form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.Map;
+
+import models.HeroSQ;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -7,5 +10,16 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        },new HandlebarsTemplateEngine());
+
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String data = request.queryParams("data");
+            HeroSQ newHero = new HeroSQ(data);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
