6b524d4cc59bca0402ce7a131d9d847911870c91
Omoyi <urakhel7@gmail.com>
2019-09-15 15:06:42 +0200

    create another directory

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
+        staticFileLocation("/public");
+
+    }
 }
