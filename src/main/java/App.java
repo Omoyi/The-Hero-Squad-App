b0c51d8c09dfa87eaa5c31797791d582f53bfb79
Omoyi <urakhel7@gmail.com>
2019-09-17 10:41:24 +0200

    change view name in App.java and do further styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,7 +27,7 @@ public class App {
 //            HeroSQ newHero = new HeroSQ(
 //                    weakPoint,heroAge,knownPower);
 //            model.put("hero", newHero);
-            return new ModelAndView(model, "execution.hbs");
+            return new ModelAndView(model, "HeroForm.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
